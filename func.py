13d486b3fd57c6ba678c85a236836a8f6a93523f
Minsun Kim <Minsun.Kim.1@stontbrook.edu>
2023-03-15 15:09:27 +0900

    Finish

--- func.py
+++ func.py
@@ -6,11 +6,22 @@ def add(x, y):
 #       neg() should return the negation of the given number, and sqrt() should
 #       return the square root of the given number. 
 
+def sub(x,y):
+    return x-y
+def div(x,y):
+    return x/y
+def mult(x,y):
+    return x*y
+def exp(x):
+    return x**2
+
 def neg(x):
-    ... # fill here
+    return -x 
+
 
 def sqrt(x):
-    ... # fill here
+    return x**0.5
+    
 
 # -------------------------------------- Task 2 -----------------------------------
 
@@ -20,9 +31,10 @@ def sqrt(x):
 a = 1
 b = -3
 c = 1
-
-x1 = ... # TODO: write a code to compute the first root of the quadratic equation
-x2 = ... # TODO: then do the same for the second root
+d= sub(exp(b),mult(4,mult(a,c))) 
+x1 = div(add(neg(b),sqrt(d)),mult(2,a))
+# TODO: write a code to compute the first root of the quadratic equation
+x2 = div(sub(neg(b),sqrt(d),mult(2,a))) # TODO: then do the same for the second root
 # Note: Make sure to remove the ellipsis (...) when you're writing your code
 
 print("First root:" + str(x1))
